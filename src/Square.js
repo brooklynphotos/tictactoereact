cbdd2dd6f430ba35e927aac2ef5a1fee2638c353
brooklynphotos <gzhong@GUONENGs-MacBook-Pro.local>
2017-10-09 00:18:29 -0400

    Now you can click and play and see who wins

--- src/Square.js
+++ src/Square.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './Square.css';
 
-class Square extends Component {
-    render() {
-        return (
-            <button className="square">
-            {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+        {props.value}
+        </button>
+    );
 }
   
 export default Square;
